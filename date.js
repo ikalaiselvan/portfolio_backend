10403d74f3c799f4d5e25bb5597075d18b1d5861
Kalaiselvan <ikalaiselvan@users.noreply.github.com>
2023-01-12 13:26:15 +0530

    first commit

---
+++ date.js
@@ -0,0 +1,20 @@
+
+
+
+let currentDate = new Date();
+let cDay = currentDate.getDate();
+let cMonth = currentDate.getMonth() + 1;
+let cYear = currentDate.getFullYear();
+let date = cDay + "-" + cMonth + "-" + cYear;
+
+let time =
+  currentDate.getHours() +
+  ":" +
+  currentDate.getMinutes() +
+  ":" +
+  currentDate.getSeconds();
+// console.log(time);
+let newDateTime = {date: date, time: time };
+// console.log(newDateTime);
+
+export default newDateTime;
---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from 'cors';
+import { MongoClient } from "mongodb";
+import newDateTime from "./date.js";
+
+// import .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+const MONGO_URL = process.env.MONGO_URL;
+const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log("Mongodb is connected ...");
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/",(req, res)=>{
+    res.send("hi this is kalai");
+})
+
+
+
+app.post("/portfolio", async function (request, response) {
+  const data = request.body;
+  const dataWithDate = {...data, ...newDateTime}
+
+  const result = await client
+    .db("portfolio")
+    .collection("html")
+    .insertOne(dataWithDate);
+
+  response.send(result);
+});
+
+app.listen(PORT, ()=>{
+    console.log("app is running on port :", PORT)
+})
+
